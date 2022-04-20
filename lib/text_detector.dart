3646c06bfbc4b7706bcce244ed544ff5ed2dd158
Markus jayjah <markus@jayjah.de>
2022-04-20 12:21:57 +0200

    rm todos and added todos code, reverted code from old deps to new deps, should finalized project -> needs to be testet

--- lib/text_detector.dart
+++ lib/text_detector.dart
@@ -1,64 +1,86 @@
-// ignore_for_file: curly_braces_in_flow_control_structures
+// ignore_for_file: curly_braces_in_flow_control_structures, unawaited_futures
 
-import 'dart:io';
-
+import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
-import 'package:google_ml_vision/google_ml_vision.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:translator/translator.dart';
+import 'package:text_in_image_detector/router.dart';
 
-class TextDetector extends StatefulWidget {
-  const TextDetector({Key? key}) : super(key: key);
+class TextDetectorWidget extends StatefulWidget {
+  const TextDetectorWidget({Key? key}) : super(key: key);
 
   @override
   _TextDetectorState createState() => _TextDetectorState();
 }
 
-class _TextDetectorState extends State<TextDetector> {
-  final List<String> textInImage = <String>[];
+class _TextDetectorState extends State<TextDetectorWidget> {
   bool loading = true;
   bool translating = false;
-
+  String? textInImage;
   String? translatedText;
 
   @override
   void initState() {
-    // TODO(jayjah): make the choise of image source available via dialog
-    pickImage(ImageSource.camera).then(readTextFromImage);
+    _initStuff();
 
     super.initState();
   }
 
+  void _initStuff({bool setNewState = false}) {
+    // reset state to initial state
+    loading = true;
+    translating = false;
+    textInImage = null;
+    translatedText = null;
+    if (setNewState) setState(() {});
+
+    // ensure it gets called on next frame
+    Future<void>.microtask(() async {
+      final ImageSource imageSource = (await AppRouter.showDialog(
+              'Pick image', 'Do you want to pick an image from gallery?'))
+          ? ImageSource.gallery
+          : ImageSource.camera;
+      pickImage(imageSource).then(readTextFromImage);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    late final Widget child;
+    // default spacer widget
+    const Widget spacer = SizedBox(
+      height: 55,
+    );
 
+    late final Widget child;
+    // build child widget from internal state
     if (loading)
       child = const Center(
         child: CircularProgressIndicator.adaptive(),
       );
-    else if (textInImage.isEmpty)
-      child = const Center(
-        child: Text('No text in images detected'),
+    else if (textInImage == null)
+      child = Column(
+        children: <Widget>[
+          spacer,
+          _closeButton,
+          const Center(
+            child: Text('No text in image detected'),
+          ),
+        ],
       );
     else if (translating)
       child = const Center(
         child: Text('Text is being translated...'),
       );
-    else {
-      const Widget spacer = SizedBox(
-        height: 55,
-      );
-
+    else
       child = SingleChildScrollView(
         child: Column(
-          children: [
-            // TODO(jayjah): add close button and re init startup method
+          children: <Widget>[
+            _closeButton,
             spacer,
             const Text('Text to translate:'),
-            for (final content in textInImage) Center(child: Text(content)),
+            if (textInImage != null) Center(child: Text(textInImage!)),
             spacer,
-            if (translatedText != null) ...[
+            if (translatedText != null) ...<Widget>[
               const Text('Translated text:'),
               Text(translatedText!)
             ] else
@@ -67,13 +89,22 @@ class _TextDetectorState extends State<TextDetector> {
           ],
         ),
       );
-    }
 
     return Material(
       child: child,
     );
   }
 
+  // A default close button, which calls [_initStuff] internally
+  Widget get _closeButton => Align(
+        alignment: Alignment.centerRight,
+        child: IconButton(
+          onPressed: () => _initStuff(setNewState: true),
+          icon: const Icon(Icons.close),
+        ),
+      );
+
+  /// Pick an image from given [source]
   Future<XFile?> pickImage(ImageSource source) {
     return ImagePicker().pickImage(source: source);
   }
@@ -81,42 +112,53 @@ class _TextDetectorState extends State<TextDetector> {
   Future<void> readTextFromImage(XFile? value) async {
     if (value == null) return;
 
-    final GoogleVisionImage visionImage =
-        GoogleVisionImage.fromFile(File(value.path));
-    final TextRecognizer textRecognizer =
-        GoogleVision.instance.textRecognizer();
-    final VisionText visionText =
-        await textRecognizer.processImage(visionImage);
+    // init google ml kit with text detector
+    final TextDetector textRecognizer = GoogleMlKit.vision.textDetector();
+    final RecognisedText recognizedText =
+        await textRecognizer.processImage(InputImage.fromFilePath(value.path));
+    textInImage = recognizedText.text;
 
-    for (TextBlock block in visionText.blocks) {
-      final String? text = block.text;
-      if (text != null) textInImage.add(text);
-    }
+    // get first detected language
+    final String detectedLanguage = recognizedText.blocks
+        .firstWhere(
+            (TextBlock element) => element.recognizedLanguages.first.isNotEmpty)
+        .recognizedLanguages
+        .first;
+    // an alternative may be:
+    // await GoogleMlKit.nlp.languageIdentifier().identifyLanguage(textInImage!);
 
-    if (mounted)
-      setState(() {
-        loading = false;
-        translating = true;
-      });
-    translateText('en');
+    // set specific new state variables
+    loading = false;
+    translating = true;
+    if (mounted) setState(() {});
+
+    textRecognizer.close();
+    translateText(fromLanguage: detectedLanguage, toLanguage: 'en');
   }
 
-  Future<void> translateText(String toLanguage) async {
-    if (textInImage.isEmpty) {
+  Future<void> translateText(
+      {required String toLanguage, required String fromLanguage}) async {
+    // just print some stuff in debug mode
+    if (kDebugMode) {
+      print('ToLanguage: $toLanguage; fromLanguage: $fromLanguage');
+    }
+
+    // ensure some text where found in the image, otherwise stop here
+    if (textInImage == null) {
       translating = false;
       if (mounted) setState(() {});
       return;
     }
 
-    String textToTranslate = '';
-    for (final toTranslate in textInImage) {
-      textToTranslate += toTranslate;
-    }
+    // translate text with google ml kit
+    final OnDeviceTranslator translator = GoogleMlKit.nlp.onDeviceTranslator(
+        sourceLanguage: fromLanguage, targetLanguage: toLanguage);
+    translatedText = await translator.translateText(textInImage!);
 
-    final translation =
-        await GoogleTranslator().translate(textToTranslate, to: toLanguage);
-    translatedText = translation.text;
+    // update state
     translating = false;
     if (mounted) setState(() {});
+
+    translator.close();
   }
 }
